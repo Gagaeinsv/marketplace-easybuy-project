714853e6f6e23458e5ce53880fd7dc31748ff428
Serhii Haharin <gagarin-sv@live.com>
2026-07-24 14:48:21 +0200

    Link SellItemBtn to /register/seller page

--- src/components/button/sell-item-btn/SellItemBtn.jsx
+++ src/components/button/sell-item-btn/SellItemBtn.jsx
@@ -1,16 +1,16 @@
 'use client';
 
-import { useLanguage } from '@/context/LanguageContext';
+import Link from 'next/link';
 
 const SellItemBtn = () => {
   const { t } = useLanguage();
   return (
-    <button
-      type="button"
-      className="font-bold flex justify-center items-center w-full max-w-[320px] h-13 text-white rounded-lg bg-orange-400 mb-15 md:mb-0 hover:brightness-110 active:scale-[0.98] transition-all duration-200 cursor-pointer"
+    <Link
+      href="/register/seller"
+      className="font-bold flex justify-center items-center w-full max-w-[320px] h-13 text-white rounded-lg bg-orange-400 mb-15 md:mb-0 hover:brightness-110 active:scale-[0.98] transition-all duration-200 cursor-pointer text-center"
     >
       {t('sellButton')}
-    </button>
+    </Link>
   );
 };
 
